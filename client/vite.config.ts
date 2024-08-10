18871029cde2372dcea2d5dd6aa5e5a92a151eb4
Николай <jetallavache@outlook.com>
2024-08-11 00:31:42 +0700

    fix(client): Added the app version to the footer

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    host: "localhost",
+    port: 8080,
+  },
   plugins: [react()],
   resolve: {
     alias: {
