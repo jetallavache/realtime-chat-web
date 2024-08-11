710de6ef926d6076a98c8b415b3c8791f291a6c1
Николай <jetallavache@outlook.com>
2024-08-11 22:40:41 +0700

    fix: semantic-release-replace-plugin

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -3,5 +3,6 @@ const { VITE_API_URL, VITE_API_WS } = import.meta.env;
 export default {
     apiUrl: VITE_API_URL || 'http://localhost:8080/',
     wsUrl: VITE_API_WS || 'ws://localhost:8080',
+    /** Do not change! */
     version: '0.0.0',
 };
